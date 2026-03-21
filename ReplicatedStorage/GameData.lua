adbf895da85fb6830108a3eafee446e6cf487c66
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-03-21 10:08:51 -0700

    Updated ReplicatedStorage/GameData.lua

--- ReplicatedStorage/GameData.lua
+++ ReplicatedStorage/GameData.lua
@@ -19,7 +19,8 @@ GameData.StyleBonuses = {
 	["Pillarman"] = { Health = 40, Strength = 20 },
 	["Ultimate Lifeform"] = { Health = 85, Defense = 55 },
 	["Spin"] = { Strength = 45, Speed = 35 },
-	["Golden Spin"] = { Strength = 85, Willpower = 55 }, 
+	["Golden Spin"] = { Strength = 85, Willpower = 55 },
+	["Rock Human"] = { Strength = 65, Defense = 40 },
 	["Man of Steel"] = { Defense = 50, Health = 50 },
 }
 
