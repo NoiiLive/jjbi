a67e60577c57a49e54d171ea8b5ab6025bf04849
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-03-28 19:18:33 -0700

    Updated StarterPlayer/StarterPlayerScripts/MobileUIDisable.lua

---
+++ StarterPlayer/StarterPlayerScripts/MobileUIDisable.lua
@@ -0,0 +1,8 @@
+-- @ScriptType: LocalScript
+local Player = game.Players.LocalPlayer
+local PlayerScripts = Player:WaitForChild("PlayerScripts")
+local PlayerModule = require(PlayerScripts:WaitForChild("PlayerModule"))
+local Controls = PlayerModule:GetControls()
+
+-- This disables the touch controls without permission errors
+Controls:Disable()
