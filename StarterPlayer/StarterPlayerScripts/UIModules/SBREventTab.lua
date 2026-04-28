b8c0d381682b87789462279d8164b2ee85d421a6
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-04-28 11:49:34 -0700

    Updated StarterPlayer/StarterPlayerScripts/UIModules/SBREventTab.lua

--- StarterPlayer/StarterPlayerScripts/UIModules/SBREventTab.lua
+++ StarterPlayer/StarterPlayerScripts/UIModules/SBREventTab.lua
@@ -768,12 +768,7 @@ function SBREventTab.Init(parentFrame, tooltipMgr, focusFunc)
 
 										vfxObj:Destroy()
 									end)
-
-									if vfxName == "Punch" or vfxName == "Slash" then
-										task.wait(0.15)
-									else
-										task.wait(0.05) 
-									end
+									task.wait(0.1)
 								end
 							end
 						end
